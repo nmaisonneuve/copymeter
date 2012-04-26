401004a971253cf0e9a5c271f9fb1168c65ba32d
nmaisonneuve <n.maisonneuve@gmail.com>
2012-04-26 12:31:10 +0200

    add some tips

--- copymeter.js
+++ copymeter.js
@@ -7,7 +7,8 @@ write_result = function (sentence) {
     var score = sentence.best_score;
     var color = (score == -1) ? "#777" : "hsl(" + (100 - (score * 100 - 20) * 100 / 80) + ", 70%, 80%)"
     $("#sentence_" + idx).css('background', color);
-    $("#sentence_" + idx).click(function () {
+    $("#sentence_" + idx).click(function (event) {
+        event.preventDefault();
         score = Math.round(score * 100) + "%";
 
         var sources = sentence.copies.slice(0, Math.min(5, sentence.copies.length));
